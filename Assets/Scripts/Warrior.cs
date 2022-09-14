fbf81be6b288ad4c20d0995345ce22a8a2e56d90
Luiz Guilherme de Souza Gonçalves <103260099+Luizgi@users.noreply.github.com>
2022-09-13 21:14:49 -0300

    Update Warrior.cs

--- Assets/Scripts/Warrior.cs
+++ Assets/Scripts/Warrior.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Warrior : MonoBehaviour
 {
-    // Start is called before the first frame update
     private Rigidbody2D Corpo;
     private Animator Anim;
+    // Start is called before the first frame update
     void Start()
     {
         Corpo = GetComponent<Rigidbody2D>();
@@ -16,16 +16,31 @@ public class Warrior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Movimento
         float velX = Input.GetAxis("Horizontal");
         Corpo.velocity = new Vector2(velX, 0);
 
-        if(velX == 0)
+        if (velX == 0)
         {
             Anim.SetBool("Andar", false);
         }
         else
         {
             Anim.SetBool("Andar", true);
+            if (velX < 0)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+            else
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
         }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Anim.SetTrigger("Attack");
+        }
+
     }
 }
